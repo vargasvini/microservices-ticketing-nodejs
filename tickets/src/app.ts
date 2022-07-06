6471f0dc85b78a8f1b4ec9a5297b2f7149062c1d
Vinícius Vargas <vargasrvinicius@gmail.com>
2022-07-06 19:35:15 -0300

    Atualizando serviço de tickets com endpoints e testes

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
-import 'express-async-errors'
+import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { NotFoundError, errorHandler } from '@goustiee-org/common';
-
+import { errorHandler, NotFoundError, currentUser } from '@goustiee-org/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,8 +17,14 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
 
-app.get('*', async ()=> {
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
