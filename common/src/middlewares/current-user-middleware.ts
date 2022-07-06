4d8b354b3f19a8f90dd7d984292fdc1ebadb75a6
Vinícius Vargas <vargasrvinicius@gmail.com>
2022-07-06 16:51:30 -0300

    Updates

--- common/src/middlewares/current-user-middleware.ts
+++ common/src/middlewares/current-user-middleware.ts
@@ -14,6 +14,9 @@ export const currentUser = (req: Request, res: Response, next: NextFunction) => 
     if(!req.session?.jwt){
         return next();
     }
+    if (!req.session?.jwt) {
+        return next();
+      }
     try {
         const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as IUserPayload;  
         req.currentUser = payload;
